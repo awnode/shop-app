983f60660882c863390cec8a35b8bdbffd55ef94
Your Name <protoncodeacc@proton.me>
2025-12-02 10:23:22 +0700

    layout

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,17 +1,59 @@
+import { FontAwesome } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
+}) {
+  return <FontAwesome size={24} {...props} style={{ color: '#1BC464' }} />;
+}
 
 const TabsLayout = () => {
   return (
-    <SafeAreaView>
-      <Tabs screenOptions={{
-        tabBarActiveTintColor: '#1BC464'
-      }}>
-        <Tabs.Screen name="index" options={{ headerShown: false }} />
-        <Tabs.Screen name="orders" options={{}} />
+    <SafeAreaView edges={['top', 'bottom']} style={styles.safeArea}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: '#1BC464',
+          tabBarInactiveTintColor: 'gray',
+          tabBarLabelStyle: { fontSize: 14 },
+          tabBarStyle: {
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            paddingTop: 10,
+          },
+          // headerShown: false
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Shop',
+            tabBarIcon(props) {
+              return <TabBarIcon {...props} name="shopping-cart" />;
+            },
+            headerShown: false,
+          }}
+        />
+        <Tabs.Screen
+          name="orders"
+          options={{
+            title: 'Orders',
+            tabBarIcon(props) {
+              return <TabBarIcon {...props} name="book" />;
+            },
+          }}
+        />
       </Tabs>
     </SafeAreaView>
   );
 };
 
 export default TabsLayout;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+});
--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,17 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native';
 
 const Home = () => {
   return (
     <View>
-      <Text>Home
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi suscipit ipsum et leo congue, et pulvinar ipsum porta. Morbi pellentesque faucibus felis sed varius. Vestibulum mollis augue non interdum fermentum. Aliquam efficitur, nulla in sagittis pulvinar, est dui gravida neque, finibus luctus mi mi in magna. Suspendisse pretium odio eros, nec facilisis purus maximus ac. Nulla ac velit eleifend felis eleifend sollicitudin et facilisis risus. Ut pulvinar viverra augue, nec aliquet lectus elementum vitae. Phasellus quis euismod elit. Vivamus posuere sem nibh. Etiam facilisis justo ut purus aliquam, in tempus ligula aliquam. Nam vulputate non mauris lacinia egestas.
-        Aliquam quis sem sit amet libero suscipit aliquam. Praesent quis mollis risus. Maecenas vel gravida libero. Nam sed suscipit felis, ac iaculis neque. Phasellus porta, lacus ut feugiat pellentesque, neque sem cursus velit, vel rhoncus magna dolor sit amet nisi. Phasellus tempus est vel fringilla auctor. Praesent ultricies nulla ipsum, ac venenatis augue ornare eu. Proin luctus eleifend sapien, sit amet consequat felis efficitur nec. Curabitur vulputate nisl lectus, non vehicula libero gravida at.
-        Praesent commodo leo urna, vitae mattis ex egestas et. Phasellus volutpat gravida lectus, id placerat sapien congue non. Pellentesque sem enim, fermentum non est et, volutpat hendrerit nisl. Phasellus velit arcu, viverra sed mauris ac, pellentesque varius augue. Etiam vestibulum ornare malesuada. Praesent consequat erat et sem sagittis dictum. Fusce quis gravida lorem, in tempus purus. Donec tempus ultricies massa, mollis condimentum metus convallis quis. Aliquam pellentesque eros vel placerat rhoncus. Integer aliquet elit non ex interdum, a faucibus ante sodales. Cras libero orci, pulvinar eget ullamcorper tincidunt, hendrerit sollicitudin tortor. Vestibulum orci tellus, rhoncus eu egestas a, congue id metus. Praesent sem quam, convallis ut tincidunt sodales, iaculis vel ante. Aenean sed dictum lectus.
-      </Text>
+      <Text>Home</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
