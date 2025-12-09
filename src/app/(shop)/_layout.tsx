b6e09c2c1918fdfb3b265454617aeae992d05731
aw <protoncodeacc@proton.me>
2025-12-09 10:56:44 +0700

    Set status bar and navbar system to black

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,16 +1,29 @@
 import { FontAwesome } from '@expo/vector-icons';
+import * as NavigationBar from 'expo-navigation-bar';
 import { Tabs } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { useEffect } from 'react';
+import { Platform, StatusBar, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
+  size: number;
 }) {
-  return <FontAwesome size={24} {...props} style={{ color: '#1BC464' }} />;
+  return <FontAwesome {...props} style={{ color: '#1BC464' }} />;
 }
 
 const TabsLayout = () => {
+  useEffect(() => {
+    NavigationBar.setButtonStyleAsync('dark');
+
+    if (Platform.OS === 'android') {
+      // Paksa status bar tidak memengaruhi layout
+      StatusBar.setTranslucent(false);
+      StatusBar.setBarStyle('dark-content');
+    }
+  }, []);
+
   return (
     <SafeAreaView edges={['top', 'bottom']} style={styles.safeArea}>
       <Tabs
@@ -23,7 +36,6 @@ const TabsLayout = () => {
             borderTopRightRadius: 20,
             paddingTop: 10,
           },
-          // headerShown: false
         }}
       >
         <Tabs.Screen
