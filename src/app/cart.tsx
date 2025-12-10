295f8f0880cc190cc3033a3a46c6c39cf912d696
aw <protoncodeacc@proton.me>
2025-12-10 08:25:06 +0700

    cart screen

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,9 +1,201 @@
-import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import {
+  Alert,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useCartStore } from '../store/cart-store';
+
+type CartItemType = {
+  id: number;
+  title: string;
+  image: any;
+  price: number;
+  quantity: number;
+};
+
+type CartItemProps = {
+  item: CartItemType;
+  onRemove: (id: number) => void;
+  onIncrement: (id: number) => void;
+  onDecrement: (id: number) => void;
+};
+
+const CartItem = ({
+  item,
+  onRemove,
+  onIncrement,
+  onDecrement,
+}: CartItemProps) => {
+  return (
+    <View style={styles.cartItem}>
+      <Image source={item.image} style={styles.itemImage} />
+      <View style={styles.itemDetails}>
+        <Text style={styles.itemTitle}>{item.title}</Text>
+        <Text style={styles.itemPrice}>${item.price.toFixed(2)}</Text>
+        <View style={styles.quantityContainer}>
+          <TouchableOpacity
+            style={styles.quantityButton}
+            onPress={() => onDecrement(item.id)}
+          >
+            <Text style={styles.quantityButtonText}>-</Text>
+          </TouchableOpacity>
+
+          <Text style={styles.itemQuantity}>{item.quantity}</Text>
+
+          <TouchableOpacity
+            style={styles.quantityButton}
+            onPress={() => onIncrement(item.id)}
+          >
+            <Text style={styles.quantityButtonText}>+</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <TouchableOpacity
+        style={styles.removeButton}
+        onPress={() => onRemove(item.id)}
+      >
+        <Ionicons name="trash-sharp" size={16} style={styles.trashBin}/>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default function Cart() {
+  const { items, removeItem, incrementItem, decrementItem, getTotalPrice } =
+    useCartStore();
+
+  const handleCheckout = () => {
+    Alert.alert('Proceeding to Checkout', `Total amount: $${getTotalPrice()}`);
+  };
+
   return (
-    <View>
-      <Text>Cart</Text>
-    </View>
-  )
+    <SafeAreaView edges={['bottom']} style={styles.safeArea}>
+      <View style={styles.container}>
+        <FlatList
+          data={items}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <CartItem
+              item={item}
+              onRemove={removeItem}
+              onIncrement={incrementItem}
+              onDecrement={decrementItem}
+            />
+          )}
+          contentContainerStyle={styles.cartList}
+        />
+
+        <View style={styles.footer}>
+          <Text style={styles.totalText}>Total: ${getTotalPrice()}</Text>
+          <TouchableOpacity
+            onPress={handleCheckout}
+            style={styles.checkOutButton}
+          >
+            <Text style={styles.checkOutButtonText}>Checkout</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 16,
+    paddingTop: 8,
+  },
+  footer: {
+    borderTopWidth: 1,
+    borderColor: '#ddd',
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+    alignItems: 'center',
+  },
+  totalText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  checkOutButton: {
+    backgroundColor: '#28a745',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 10,
+  },
+  checkOutButtonText: {
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  cartList: {},
+  safeArea: {
+    flex: 1,
+  },
+  cartItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+    padding: 10,
+    borderRadius: 8,
+    backgroundColor: '#f9f9f9',
+  },
+  itemImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 8,
+  },
+  itemDetails: {
+    flex: 1,
+    paddingLeft: 16,
+  },
+  itemTitle: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+    fontSize: 18,
+  },
+  itemPrice: {
+    color: 'grey',
+    fontSize: 16,
+    marginBottom: 4,
+  },
+  quantityContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  itemQuantity: {
+    fontSize: 14,
+    marginHorizontal: 4,
+  },
+  quantityButton: {
+    backgroundColor: '#ddd',
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 5,
+  },
+  quantityButtonText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  removeButton: {
+    backgroundColor: '#fc5050ff',
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  trashBin: {
+    color: '#fff'
+  }
+});
