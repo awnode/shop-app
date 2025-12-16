6e8bcc8a11bd7eafbcf2c5c9c84732e9df508d75
aw <protoncodeacc@proton.me>
2025-12-16 09:43:06 +0700

    login screen

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,24 +1,24 @@
 import { PRODUCTS } from '@/assets/products';
 import { ListHeader } from '@/src/components/list-header';
+import { ProductListItem } from '@/src/components/product-list-item';
 import { FlatList, StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { ProductListItem } from '../../components/product-list-item';
 
 const Home = () => {
   return (
     <SafeAreaView edges={['top']}>
-    <View>
-      <FlatList
-        data={PRODUCTS}
-        renderItem={({ item }) => <ProductListItem product={item} />}
-        keyExtractor={(item) => item.id.toString()}
-        numColumns={2}
-        ListHeaderComponent={ListHeader}
-        contentContainerStyle={styles.flatListContent}
-        columnWrapperStyle={styles.flatListColumn}
-        style={{ paddingHorizontal: 10, paddingVertical: 5 }}
-      />
-    </View>
+      <View>
+        <FlatList
+          data={PRODUCTS}
+          renderItem={({ item }) => <ProductListItem product={item} />}
+          keyExtractor={(item) => item.id.toString()}
+          numColumns={2}
+          ListHeaderComponent={ListHeader}
+          contentContainerStyle={styles.flatListContent}
+          columnWrapperStyle={styles.flatListColumn}
+          style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -1,9 +1,191 @@
-import { Text, View } from 'react-native';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Controller, useForm } from 'react-hook-form';
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import * as zod from 'zod';
+
+const authSchema = zod.object({
+  email: zod.email({ error: 'Invalid email address' }),
+  password: zod
+    .string()
+    .min(6, { error: 'Password must be at least 6 charcters long' }),
+});
 
 export default function Auth() {
+  const { control, handleSubmit, formState } = useForm({
+    resolver: zodResolver(authSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  // useEffect(() => {
+  //   if (Platform.OS === 'android') {
+  //     // Paksa status bar tidak memengaruhi layout
+  //     // StatusBar.setTranslucent(false);
+  //     StatusBar.setBarStyle('light-content');
+  //   }
+  // }, []);
+
+  const signIn = (data: zod.infer<typeof authSchema>) => {
+    console.log(data);
+  };
+
+  const signUp = (data: zod.infer<typeof authSchema>) => {
+    console.log(data);
+  };
+
   return (
-    <View>
-      <Text>Auth</Text>
-    </View>
-  )
+    <SafeAreaView edges={['top']} style={{ flex: 1 }}>
+      <ImageBackground
+        source={{
+          uri: 'https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        }}
+        style={styles.backgroundImage}
+      >
+        <View style={styles.overlay} />
+
+        <View style={styles.container}>
+          <Text style={styles.title}>Welcome</Text>
+          <Text style={styles.subtitle}>Please Authenticate to continue</Text>
+
+          <Controller
+            control={control}
+            name="email"
+            render={({
+              field: { value, onChange, onBlur },
+              fieldState: { error },
+            }) => (
+              <>
+                <TextInput
+                  placeholder="Email"
+                  style={styles.input}
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  placeholderTextColor="#aaa"
+                  autoCapitalize="none"
+                  editable={!formState.isSubmitting}
+                />
+                {error && <Text style={styles.error}>{error.message}</Text>}
+              </>
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password"
+            render={({
+              field: { value, onChange, onBlur },
+              fieldState: { error },
+            }) => (
+              <>
+                <TextInput
+                  placeholder="Password"
+                  style={styles.input}
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  secureTextEntry
+                  placeholderTextColor="#aaa"
+                  autoCapitalize="none"
+                  editable={!formState.isSubmitting}
+                />
+                {error && <Text style={styles.error}>{error.message}</Text>}
+              </>
+            )}
+          />
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={handleSubmit(signIn)}
+            disabled={formState.isSubmitting}
+          >
+            <Text style={styles.buttonText}>Sign In</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.button, styles.signUpButton]}
+            onPress={handleSubmit(signUp)}
+            disabled={formState.isSubmitting}
+          >
+            <Text style={styles.buttonText}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0,0,0,0.7)',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+    width: '100%',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 36,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#ddd',
+    marginBottom: 32,
+  },
+  input: {
+    width: '90%',
+    padding: 12,
+    marginBottom: 16,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    fontSize: 16,
+    color: '#000',
+  },
+  error: {
+    color: 'red',
+    fontSize: 12,
+    marginBottom: 16,
+    textAlign: 'left',
+    width: '90%',
+  },
+  button: {
+    backgroundColor: '#7b00ff',
+    width: '90%',
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 16,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  signUpButton: {
+    backgroundColor: 'transparent',
+    borderColor: '#fff',
+    borderWidth: 1,
+  },
+});
